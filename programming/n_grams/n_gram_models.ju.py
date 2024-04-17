d91be446a9517404788fb0cec4c28ad462a1f779
ducks4rule <106486452+ducks4rule@users.noreply.github.com>
2024-04-17 17:57:26 +0200

    n grams almost working

--- programming/n_grams/n_gram_models.ju.py
+++ programming/n_grams/n_gram_models.ju.py
@@ -15,7 +15,7 @@ loading the data
 """
 
 # %%
-path = '/home/lorenz/Documents/Mathematik/24 FS/Semester_Paper_DCML/data/ABC/harmonies/'
+path = '~/Documents/Mathematik/24 FS/Semester_Paper_DCML/data/ABC/harmonies/'
 file = 'n10op74_01.harmonies.tsv'
 df_all = pd.read_csv(path + file, sep='\t')
 
@@ -33,19 +33,22 @@ N-gram model
 # %%
 
 class NGramModel:
-    def __init__(self, df, n_gram_=2):
+    def __init__(self, df, n_gram_: int = 2):
         self.df = df
+        self.df_ = pd.factorize(df)
+        assert(n_gram_ > 1)
         self.n_gram = n_gram_
 
         self.clean_data()
 
+        self.input_df = self.generate_n_grams(self.df, self.n_gram-1)
+        self.input_df_ = pd.factorize(self.input_df)
         self.data = self.generate_n_grams(self.df, self.n_gram)
         self.data_ = pd.factorize(self.data)
     
     def clean_data(self):
         # if df_ contains a None value, remove it and the corresponding entry in df
-        df_ = pd.factorize(self.df)
-        if None in df_[1]:
+        if None in self.df_[1]:
             ind = np.argwhere(df_[1] is None)
             self.df = np.delete(self.df, ind)
 
@@ -60,54 +63,76 @@ class NGramModel:
 
     # calculate the transition matrix
     def transition_probs(self):
-        trans_mat = np.zeros((len(self.data_[1]), len(self.data_[1])))
+        trans_mat = np.zeros((len(self.input_df_[1]), len(self.df_[1])))
 
         # count of transitions
-        for ch1 in self.data_[1]:
-            inds = np.argwhere(self.df == ch1).flatten()
-            # if first entry = 0, remove it 
-            if inds[0] == 0:
-                inds = np.delete(inds, 0)
-            for ch2 in self.data_[1]:
-                before = sum([chd == ch2 for chd in self.df[inds-1]])
-                trans_mat[self.data_[1] == ch1, self.data_[1] == ch2] = before/len(self.df[inds])
-        self.trans_mat = trans_mat
+        # data_split = np.array([tok.split()[:self.n_gram-1] for tok in self.data_[1]])
+        for ch_lab, chords in enumerate(self.input_df_[1]):
+            inds_chords = np.argwhere(self.input_df == chords).flatten()
+            data_short = []
+            labels_data = []
+            for i, dat in enumerate(self.data_[1]):
+                if dat.startswith(chords):
+                    data_short.append(dat)
+                    labels_data.append(i)
+                
+            before_count = len(inds_chords)
+            followers = [tok.split()[-1] for tok in data_short]
+            ind_followers = [np.argwhere(self.df_[1] == f)[0][0] for f in followers]
+            followers_count = [sum(data_s == self.data) for data_s in data_short]
+            followers_prob = [f_c/before_count for f_c in followers_count]
+            for j, ind in enumerate(ind_followers):
+                trans_mat[ch_lab, ind] = followers_prob[j]
         return trans_mat
 
     def transform_chords_to_vectors(self, ch):
-        assert(ch in self.data_[1])
-        ind =  np.argwhere(self.data_[1] == ch)[0]
-        vec = np.zeros(len(self.data_[1]))
+        assert(ch in self.input_df_[1])
+        ind =  np.argwhere(self.input_df_[1] == ch)[0]
+        vec = np.zeros(len(self.input_df_[1]))
         vec[ind] = 1
         return vec
 
     def fit(self):
         self.clean_data()
-        self.trans_mat = self.markov_transition_matrix()
+        self.trans_mat = self.transition_probs()
         return self
 
     def step(self, ch):
-        # print('step form ', ch)
-        vec = self.transform_chords_to_vectors(ch)
+        if len(ch.split()) == self.n_gram-1:
+            print('simple step')
+            return self.simple_step(ch)
+        elif len(ch.split()) == 1:
+            # TODO markov chain
+            print('TODO this is where the markov chain should be')
+        else:
+            temp_mod = NGramModel(self.df, n_gram_=len(ch.split()) + 1)
+            print('difficult step')
+            return temp_mod.fit().step(ch)
+
+    def simple_step(self, chs):
+        vec = self.transform_chords_to_vectors(chs)
         out = np.dot(self.trans_mat.T, vec)
-        return np.random.choice(self.data_[1], 1, p=out)[0]
+        return np.random.choice(self.df_[1], 1, p=out)[0]
 
-    def predict(self, ch, n=3, verbose=False, start_at_current=False):
-        seq = []
-        if start_at_current:
-            seq.append(ch)
-
+    def predict(self, ch, n=5, verbose=False):
+        seq = [ch]
         for i in range(n):
-            ch = self.step(ch)
-            seq.append(ch)
+            print('step ', i)
+            ch_temp = self.step(ch)
+            seq.append(ch_temp)
+            from_i = min(i, self.n_gram-1)
+            ch = ' '.join(seq[-from_i:])
+            print('ch ', ch)
             if verbose:
                 print(ch)
         return seq
 
 
 # %%
-n_gram = NGramModel(df, n_gram_=3)
-print(n_gram.data_)
+n_gram = NGramModel(df, n_gram_=4)
+n_gram.fit()
+pred = n_gram.predict('I V',n=5, verbose=True)
+print('seq ', pred)
 
 # %% [markdown]
 """
