b51c61c06fe2c2cbbf1adf78d48251bbb76558a8
ducks4rule <106486452+ducks4rule@users.noreply.github.com>
2024-05-03 11:18:58 +0200

    bad plots

--- programming/main.py
+++ programming/main.py
@@ -6,7 +6,6 @@ from load_data import construct_data
 from metrics import calculate_metrics
 import utils
 import pickle
-import subprocess
 
 
 
@@ -28,40 +27,59 @@ scores_bin = np.zeros(pred_n)
 all_scores_tbt = {}
 all_scores_bin = {}
 
-for n_ in n:
-    for sqtt_number_ in sqtt_number:
-        for inversion in inversions:
-            for j_ in range(repeats):
-                df = df_all[sqtt_number_]
-                context = df[:n_context]
-                ground_truth = df[n_context:n_context + pred_n]
-                model = NGram(df_all, n=n_, inversions=inversion)
-                model.fit()
-                file_name = 'test'
-                pred = model.predict(context, pred_n, verbose=True)
-                utils.save_predictions_parameters_n_gram(pred, ground_truth=ground_truth, model=model, context=context, file_name_ext=file_name, verbose=True)
-                calculate_metrics(file_name=file_name, number=j_, sqtt_number=sqtt_number_, with_sps_metric=False, with_inversion=inversion)
-                # print('n:', n_, 'sqtt_number:', sqtt_number_, 'inversion:', inversion, 'repeat:', j_)
-                scores_tbt += np.load('scores/scores_tone_by_tone.npy')/repeats
-                scores_bin += np.load('scores/scores_binary.npy')/repeats
-            entry = f"n: {n_}, sqtt_number: {sqtt_number_}, inversion: {inversion}"
-            all_scores_tbt[entry] = scores_tbt
-            all_scores_bin[entry] = scores_bin
+calculate_switch = True
 
-# save the dictionaries
-with open('scores/all_scores_tbt.pkl', 'wb') as f:
-    pickle.dump(all_scores_tbt, f)
-with open('scores/all_scores_bin.pkl', 'wb') as f:
-    pickle.dump(all_scores_bin, f)
+if calculate_switch:
+    for n_ in n:
+        for sqtt_number_ in sqtt_number:
+            for inversion in inversions:
+                for j_ in range(repeats):
+                    df = df_all[sqtt_number_]
+                    context = df[:n_context]
+                    ground_truth = df[n_context:n_context + pred_n]
+                    model = NGram(df_all, n=n_, inversions=inversion)
+                    model.fit()
+                    file_name = 'test'
+                    pred = model.predict(context, pred_n, verbose=True)
+                    utils.save_predictions_parameters_n_gram(pred, ground_truth=ground_truth, model=model, context=context, file_name_ext=file_name, verbose=True)
+                    calculate_metrics(file_name=file_name, number=j_, sqtt_number=sqtt_number_, with_sps_metric=False, with_inversion=inversion)
+                    # print('n:', n_, 'sqtt_number:', sqtt_number_, 'inversion:', inversion, 'repeat:', j_)
+                    scores_tbt += np.load('scores/scores_tone_by_tone.npy')
+                    scores_bin += np.load('scores/scores_binary.npy')
+                entry = f"n: {n_}, sqtt_number: {sqtt_number_}, inversion: {inversion}"
+                all_scores_tbt[entry] = scores_tbt/repeats
+                all_scores_bin[entry] = scores_bin/repeats
+
+    # save the dictionaries
+    with open('scores/all_scores_tbt.pkl', 'wb') as f:
+        pickle.dump(all_scores_tbt, f)
+    with open('scores/all_scores_bin.pkl', 'wb') as f:
+        pickle.dump(all_scores_bin, f)
+
+# load the dictionaries
+with open('scores/all_scores_tbt.pkl', 'rb') as f:
+    all_scores_tbt = pickle.load(f)
+with open('scores/all_scores_bin.pkl', 'rb') as f:
+    all_scores_bin = pickle.load(f)
 
 # print the dictionaries
 print(all_scores_tbt)
 print(all_scores_bin)
 
-# plot the dictionaries with plt 
-plt.bar(all_scores_tbt.keys(), all_scores_tbt.values())
+# plot the results with plt 
+plt.figure()
+for key, value in all_scores_tbt.items():
+    plt.plot(value, label=key)
+plt.legend()
+plt.title('Tone by tone')
+plt.savefig('scores/tone_by_tone.png')
 plt.show()
 
-plt.bar(all_scores_bin.keys(), all_scores_bin.values())
+plt.figure()
+for key, value in all_scores_bin.items():
+    plt.plot(value, label=key)
+plt.legend()
+plt.title('Binary')
+plt.savefig('scores/binary.png')
 plt.show()
 
